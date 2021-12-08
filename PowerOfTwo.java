4c7c1fae3b8161bba708455118217c6879cd0ce5
Eric Lin <ericlin071906@gmail.com>
2021-12-08 17:32:03 -0500

    added PowerOfTwo.Java

---
+++ PowerOfTwo.java
@@ -0,0 +1,16 @@
+public class PowerOfTwo {
+    public static void main(String[] args){
+	    poweroftwo();
+    }
+
+
+    public static void poweroftwo(){
+        int target = 694;
+        int num = 2;
+        while (num < 694){
+            System.out.println(num);
+            num = num * 2;
+        }
+
+    }
+}
